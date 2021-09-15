22525a3e348e32b266c4118313cf115280a9cb72
hash <hash@zip.s19.be>
2021-09-15 18:20:34 +0200

    sorte five

--- sort.c
+++ sort.c
@@ -12,7 +12,7 @@ void len_three(t_st *a)
         ft_swap_s(a);
         write(1, "sa\n", 3);
         ft_swap_reverse_rotate(a);
-        write(1, "rra\n", 3);
+        write(1, "rra\n", 4);
     }
     else if (a->stack[0] > a->stack[1] && a->stack[1] < a->stack[1])
     {
@@ -29,8 +29,18 @@ void len_three(t_st *a)
     else
     {
         ft_swap_reverse_rotate(a);
-        write(1, "rra\n", 3);
+        write(1, "rra\n", 4);
     }
 }
 
-
+void len_five(t_st *a, t_st *b)
+{
+    // ft_swap_pa(b, a);
+    // ft_swap_pa(b, a);
+    // len_three(a);
+    // ft_swap_pb(a, b);
+    // if (a->stack[0] > a->stack[a->len])
+    //     ft_swap_rotate(a);
+    
+    //j'ai une autre idée :D
+}
