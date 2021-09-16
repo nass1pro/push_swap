830b2316b3ee03d3495eea4b2dda7ac530bc3714
hash <hash@zip.s19.be>
2021-09-16 15:14:31 +0200

    quiiiiiiick sooooooooooort whouhouuuuu

--- sort.c
+++ sort.c
@@ -34,12 +34,29 @@ void len_three(t_st *a)
         write(1, "rra\n", 4);
     }
 }
-void len_five(t_st *a, t_st *b)
-{
-    ft_swap_pa(b, a);
-    ft_swap_pa(b, a);
-    len_three(a);
-    ft_swap_pb(a, b);
-    if (a->stack[0] > a->stack[a->len])
-        ft_swap_rotate(a);
-}
+// void len_five(t_st *a, t_st *b)
+// {
+//     int i;
+
+//     i = 0;
+//     ft_swap_pb(b, a)
+//     ft_swap_pb(b, a);
+//     len_three(a);
+
+//     while (b->len)
+//     {
+//         while(i < a->len && a->stack[i] < b->stack[0])
+//         {
+//             i++;
+//         }
+//         ft_swap_pa(a, b);
+
+
+
+//         while(i < a->len)
+//         {
+
+//         }
+//     }
+
+// }
