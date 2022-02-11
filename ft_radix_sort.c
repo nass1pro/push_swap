dc2f279efe69cc4094e4e25a64506f81eae419bd
hash <hash@Tchuuk.local>
2022-02-11 17:41:49 +0100

    radix

--- ft_radix_sort.c
+++ ft_radix_sort.c
@@ -1,2 +1,27 @@
 #include "push_swap.h"
 
+int ft_radix(t_st *a , t_st *b)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (true)
+    {
+        j = 0;
+        while (j < a->len)
+        {
+            if ((a->first >> i)&1)
+                ft_swap_rotate_a(a);
+            else
+                ft_swap_pb(b, a);
+            j++;
+        }
+        while(b->len)
+            ft_swap_pa(a, b);
+        if (ft_verif_sort(a) == -1)
+            break;
+        i++;
+    }
+}
